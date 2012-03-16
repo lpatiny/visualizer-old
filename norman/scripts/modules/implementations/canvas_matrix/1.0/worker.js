131c2de1856933513962e69acdf52ead58eec033
Christopher Little <hephistocles@gmail.com>
2012-03-16 15:57:14 -0700

    Add/fix the ability to have any dimensions for the cell. Also try to prevent extra drawing happening when it doesn't appear on the canvas.

--- norman/scripts/modules/implementations/canvas_matrix/1.0/worker.js
+++ norman/scripts/modules/implementations/canvas_matrix/1.0/worker.js
@@ -3,32 +3,19 @@ function generateGrid(gridData, gridImage, cols, rows, cellWidth, cellHeight) {
 	
 	var dataColumns = gridData.length;
 	var gridImageData = gridImage.data;
+	var gridWidth = gridImage.width;
 	
-	//var ctx = document.getElementsByTagName("canvas")[0].getContext('2d');
 	var color, x=0, y=0, i=0, j=0, pixelNum=0;
 	
 	while (x<cols) {
-		//gridData[x/cellWidth] = [];
+	
 		while (y<rows) {
-			
-			
+		
+			//color = Math.floor( Math.random() * 255 );
 			color = Math.round(gridData[x][y].value * 255);
 			
-		//	color = Math.floor( Math.random() * 255 );
-		//	gridData[x/cellWidth][y/cellHeight] = color;
-			while (i<cellWidth) {
-				while (j<cellHeight) {
-					pixelNum = 4 * (x * cellWidth + i + (y * cellHeight + j) * dataColumns * cellHeight);
-					gridImageData[pixelNum+0] = color; // Red value
-					gridImageData[pixelNum+1] = color; // Green value
-					gridImageData[pixelNum+2] = color; // Blue value
-					gridImageData[pixelNum+3] = 255; // Alpha value
-					j++;
-				}
-				j=0;
-				i++;
-			}
-			i=0;
+			// The Math.min ensures that we don't try to draw beyond the right edge of the canvas
+			drawCell(x*cellWidth, y*cellHeight, Math.min(cellWidth,gridWidth - x*cellWidth), cellHeight, gridWidth, color, gridImageData);
 			y++;
 		}
 		y=0;
@@ -37,7 +24,60 @@ function generateGrid(gridData, gridImage, cols, rows, cellWidth, cellHeight) {
 	
 	return gridImage;
 }
+     
+function generateGridArea(gridData, gridImage, startCol, startRow, endCol, endRow, cellWidth, cellHeight) {
+	
+	var dataColumns = gridData.length;
+	var gridImageData = gridImage.data;
+	var gridWidth = gridImage.width;
+	var gridHeight = gridImage.height;
+	
+	debugger;
+	
+	
+	var color, x=startCol, y=startRow, i=0, j=0, pixelNum=0;
+	
+	while (x<endCol) {
+	
+		while (y<endRow) {
+		
+			color = Math.round(gridData[x][y].value * 255);
+						
+			// The Math.min calls ensure that we don't try to draw beyond the edges of the canvas
+			drawCell(x*cellWidth, y*cellHeight, Math.min(cellWidth, gridWidth - x*cellWidth), Math.min(cellHeight, gridHeight - y*cellHeight), gridWidth, color, gridImageData);
+			y++;
+		}
+		y=startRow;
+		x++;
+	}
+	
+	return gridImage;
+}
+
+
+function drawCell(startX, startY, cellWidth, cellHeight, gridWidth, color, gridImageData) {
+	
+	//cellWidth or Height will be <0 when the cell is off the edge of the screen, due to the use of Math.min
+	if ((cellWidth<0) || (cellHeight<0))
+		return;
+		
+	var i=0, j=0, pixelNum = 0;
+	while (j<cellHeight) {
+		while (i<cellWidth) {
+			pixelNum = 4 * (startX + i + (startY + j) * gridWidth);
+			gridImageData[pixelNum+0] = color; // Red value
+			gridImageData[pixelNum+1] = color; // Green value
+			gridImageData[pixelNum+2] = color; // Blue value
+			gridImageData[pixelNum+3] = 255; // Alpha value
+			i++;
+		}
+		i=0;
+		j++;
+	}
+	j=0;
+}	
 
 onmessage = function(event) {
 	postMessage(generateGrid(event.data.gridData, event.data.gridImageData, event.data.cols, event.data.rows, event.data.cellWidth, event.data.cellHeight));
+	//postMessage(generateGridArea(event.data.gridData, event.data.gridImageData, 0,0,15,15, 20, 20));
 }
