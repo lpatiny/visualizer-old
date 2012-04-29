82f38279c3b9b508c0e13013a2d75f2be3c9cc7b
Norman Pellet <norman.pellet@gmail.com>
2012-04-28 20:34:11 -0700

    Ok the main should work like this, more or less. But we need to study clearly how to save data such as scripts or results

--- main/javascript/run.js
+++ main/javascript/run.js
@@ -1,37 +1,49 @@
-var help={};
-var helpFiles = ["ChemCalc","String"];
-for (var i=0; i<helpFiles.length; i++) {
-	$.getJSON("http://script.epfl.ch/servletScript/javascript/help/"+helpFiles[i]+".help.json", {}, function(data) {
-		for (property in data) {
-			help[property]=data[property];
-		}
-	});
-}
 
-var url='JavaScriptServlet';
+var url = 'http://script.epfl.ch/servletScript/JavaScriptServlet';
+var result;
+var help = {};
 
-function openVisualizer() {
-	// do we have a data and view ?
-	var viewURL="";
-	var dataURL="";
-	var saveViewURL="";
-	if (! result.url) {
+
+var visualizer;
+
+(function() {
+	var helpFiles = ["ChemCalc","String"];
+	for (var i=0; i < helpFiles.length; i++) {
+		$.getJSON("http://script.epfl.ch/servletScript/javascript/help/"+helpFiles[i]+".help.json", {}, function(data) {
+			for (property in data) {
+				help[property]=data[property];
+			}
+		});
+	}
+})();
+
+
+function openVisualizer(newWindow) {
+	var viewURL = '', dataURL = '', saveViewURL = '';
+	if (!result.url) {
 		alert("No results are available");
 		return;
-	} else {
-		dataURL=escape(result.url);
-	}
-	if ($('#viewList').val() && $('#viewList').val()!="") {
-		viewURL=escape($('#viewList').val());
-		saveViewURL=escape($('#viewList').val().replace("action=LoadFile","action=SaveFile"));
-	} else {
-		alert("The view is not specified");
+	} else
+		dataURL = escape(result.url);
+	
+	if(typeof visualizer == "undefined") {
+		alert("The visualizer has not been chosen");
 		return;
 	}
 	
-	window.open("http://lpatiny.github.com/visualizer/index.html?viewURL="+viewURL+"&dataURL="+dataURL+"&saveViewURL="+saveViewURL);
+	if(visualizer != null) {
+		viewURL = escape(visualizer);
+		saveViewURL = escape(visualizer.replace("action=LoadFile","action=SaveFile"));
+	}
+	
+	var url = ["http://lpatiny.github.com/visualizer/index.html?viewURL=", viewURL, "&dataURL=", dataURL, "&saveViewURL=", saveViewURL].join('');
+	if(newWindow)
+		window.open(url);
+	else
+		window.document.location.href = url;
 }
 
+/*
 function saveScript() {
 	if ($('#name').val()=="") {
 		alert("You need to specifiy the name of the script you want to save.");
@@ -51,7 +63,9 @@ function saveScript() {
 			refreshScript();
 	  });
 }
+*/
 
+/*
 function saveView() {
 	if ($('#name').val()=="") {
 		alert("You need to specifiy the name of the view you want to save.");
@@ -71,7 +85,9 @@ function saveView() {
 			refreshScript();
 	  });
 }
+*/
 
+/*
 function deleteScript() {
 	if ($('#name').val()=="") {
 		alert("You need to specifiy the name of the file you want to delete.");
@@ -91,98 +107,36 @@ function deleteScript() {
 	}
 
 }
+*/
 
-var result;
-function runScript() {
+function runScript(callback) {
 	editor.save();
-	$('#result').val("Running ...");
-	hideButtons();
-	$.post(url,
-			{
-				script: $('#script').val()
-			},
-			function(data) {
-				result=data;
-				$('#result').val(JSON.stringify(data));
-				for (var metaVar in result){
-					if (result[metaVar].type == 'matrix'){
-						$('#matdraw').css('visibility','visible');
-						$('#report').css('visibility','visible');
-					}
-					if(result[metaVar].type == 'dendrogram')
-						$('#dendrogram').css('visibility','visible');
-					if(result[metaVar].type == 'jcamp')
-						$('#jcamp').css('visibility','visible');
-					if(result[metaVar].type == 'googleChart')
-						$('#googleChart').css('visibility','visible');
-				}
-		  });
+	$.post(url, { script: $('#script').val() }, function(data) {
+		result = JSON.parse(data);
+		$('#output').val(data);
+		if(typeof callback == "function")
+			callback();
+		checkFinalStep();
+	 });
 	
 }
 
-
-function hideButtons(){
-	$('#matdraw').css('visibility','hidden');
-	$('#report').css('visibility','hidden');
-	$('#dendrogram').css('visibility','hidden');
-	$('#jcamp').css('visibility','hidden');
-	$('#googleChart').css('visibility','hidden');
-}
-
 function loadScript(scriptName) {
-	$('#name').val(scriptName);
-	$.get(url,
-		{
-			action: "LoadScript",
-			name:scriptName
-		},
-		function(data) {
-			editor.toTextArea();
-			$('#script').val(data);
-			addEditor();
+	
+	$.get(url, {  action: "LoadScript",
+		      name: scriptName
+		      
+	}, function(data) {
+		editor.toTextArea();
+		$('#script').val(data);
+		addEditor();
 	  });
 }
 
-function refreshView() {
-	$.get(url,
-		{
-			action: "ListView"
-		},
-		function(data) {
-			var lines=data.split(/[\r\n]+/);
-			$('#viewList').html("");
-			for (var i=0; i<lines.length; i++) {
-				if (lines[i]!="") {
-					var fields=lines[i].split("\t");
-					if (fields.length==2) {
-						var option = document.createElement('option');
-						option.value=fields[0];
-						option.appendChild(document.createTextNode(fields[1]));
-						$('#viewList').append(option);
-					}
-				}
-			}
-	  });
-}
-
-function refreshScript() {
-	$.get(url,
-		{
-			action: "ListScript"
-		},
-		function(data) {
-			var lines=data.split(/[\r\n]+/);
-			$('#scriptList').html("");
-			for (var i=0; i<lines.length; i++) {
-				if (lines[i]!="") {
-					var option = document.createElement('option');
-					option.value=lines[i];
-					option.appendChild(document.createTextNode(lines[i]));
-					$('#scriptList').append(option);
-				}
-			}
-	  });
-	refreshView();
+function selectVisualizer(visualizerName) {
+	$(".selectedvisualizer").removeClass('none').html('You have selected the following visualizer : ' + visualizerName);
+	visualizer = visualizerName;
+	checkFinalStep();
 }
 
 var editor;
@@ -190,7 +144,6 @@ var hints;
 
 function addEditor() {
       hints = $("#hints");
-  
       editor = CodeMirror.fromTextArea($("#script")[0], {
         mode: "javascript",
         onCursorActivity: function(cm) {HELP.showHints(cm, hints);},
@@ -203,9 +156,151 @@ function addEditor() {
 }
 
 
-$(document).ready(function() {
-	refreshScript();
-	addEditor();
+function addFileBrowser() {
+	var mainUrl = 'http://script.epfl.ch/servletScript/JavaScriptServlet?action=FileManager&event=list&target=data';
 	
-	FileBrowser.initialize();
+	function jsonToDynatree(data) {
+		var els = [];
+		
+		for(var i in data.result) {
+			var el = {};
+			if(typeof data.result[i] == 'object') {
+				el.isFolder = true;
+				for(var j in data.result[i]) {
+					el.key = j;
+					el.title = j;
+				}
+				
+				el.isLazy = true;
+			} else {
+				el.key = data.result[i];
+				el.title = data.result[i];
+			}
+			
+			els.push(el);
+		}
+		
+		return els;
+	}
+	
+	$(".browser").each(function() {
+		
+		
+		var div = $(this);
+		var mode = div.data('mode');
+		$.getJSON(mainUrl, {}, function(data) {
+			var els = jsonToDynatree(data);
+			div.dynatree({
+				children: els,
+				onLazyRead: function(node) {
+					var key = node.data.key;
+					
+					$.getJSON(mainUrl + "&name=" + key, {}, function(data) {
+						var els = jsonToDynatree(data);
+						node.addChild(els);
+					});
+				},
+				
+				onActivate: function(node) {
+					if(node.isFolder)
+						return;
+						
+					var path = [];
+					while(node.parent) {
+						path.unshift(node.data.key);
+						node = node.parent;
+					}
+					
+					path = path.join('/');
+					switch(mode) {
+						case 'scripts':
+							loadScript(path);
+						break;
+						case 'visualizers':
+							selectVisualizer(path);
+						break;
+					}
+				}
+			});
+		});
+	});	
+}
+
+function checkFinalStep() {
+	$("#buttons4 > *")[(result && typeof visualizer != "undefined") ? 'removeClass' : 'addClass']('disabled');
+}
+
+$(document).ready(function() {
+	//refreshScripts();
+	addEditor();
+	addFileBrowser();
+	checkFinalStep();
+	
+	$("#buttons2 [rel=launch]").bind('click', function() {
+		var btn = $(this);
+		if(btn.hasClass('disabled'))
+			return;
+		btn.data('html', btn.html()).html('Script is currently running').addClass('disabled');
+		runScript(function() {
+			btn.html(btn.data('html')).removeClass('disabled');
+			btn.siblings().removeClass('disabled');
+		});
+		
+		btn.siblings().each(function() {
+			var html, btn = $(this);
+			if(html = btn.data('html'))
+				btn.html(html).data('html', false);
+		});
+	});
+	
+	$("#buttons2 [rel=mail]").bind('click', function() {
+		var btn = $(this);  
+		btn.data('html', btn.html()).html("Report is being sent...");
+		$.ajax({
+			url: url + "&sendReport=true", 
+			type: 'post',
+			data: { output: $("#ouput").val() }, 
+			
+			success: function() {
+				btn.html('The report has been sent !');
+			},
+			
+			error: function() {
+				btn.html('Error while sending the report');
+			}
+		});
+	});
+	
+	$("#visualizernew").bind('click', function() {
+		var btn = $(this);
+		if(btn.hasClass('selected')) {
+			visualizer = _oldVisualizer;
+			
+			if(visualizer)
+				$(".selectedvisualizer").removeClass('none');
+				
+			btn.removeClass('selected');
+			checkFinalStep();
+		} else {
+			
+			_oldVisualizer = visualizer;
+			visualizer = null;
+			$(".selectedvisualizer").addClass('none');
+			btn.addClass('selected');
+			checkFinalStep();
+		}	
+	});
+	
+	$("#buttons4 [rel=open]").bind('click', function() {
+		if($(this).hasClass('disabled'))
+			return;
+		openVisualizer(false);
+	});
+	
+	$("#buttons4 [rel=opennew]").bind('click', function() {
+		if($(this).hasClass('disabled'))
+			return;
+		openVisualizer(true);
+	});
+	
 });
