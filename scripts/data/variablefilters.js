46a196704a4ac548d5e67d9ac87cc3cba6972c0b
Norman Pellet <norman.pellet@gmail.com>
2012-09-04 13:01:37 +0200

    Commit filter

--- scripts/data/variablefilters.js
+++ scripts/data/variablefilters.js
@@ -11,8 +11,19 @@ CI.VariableFilters = {
 
 		process: function(value, output) {
 
+			dfd = $.Deferred();
 
+			$.ajax({
+				dataType: 'json',
+				type: 'get',
+				url: 'http://www.chemcalc.org/service?action=em2mf&mfRange=C1-20H1-40O1-10N0-10&monoisotopicMass=' + value,
 
+				sucess: function(val) {
+					dfd.resolve(val.results);
+				}
+			});
+
+			return dfd.promise();
 		}
 	}
 
