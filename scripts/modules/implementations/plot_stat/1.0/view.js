b1f4bcaf8975827b266a76b9c85c0e16a306bd44
Norman Pellet <norman.pellet@gmail.com>
2012-09-23 13:56:38 +0200

    add y axis min/max

--- scripts/modules/implementations/plot_stat/1.0/view.js
+++ scripts/modules/implementations/plot_stat/1.0/view.js
@@ -66,93 +66,94 @@ CI.Module.prototype._types.plot_stat.View.prototype = {
 	typeToScreen: {
 
 		'chart': function(deferred, moduleValue) {
-				var view = this;
-				var cfg = this.module.getConfiguration();
-	
-				moduleValue = CI.DataType.getValueIfNeeded(moduleValue);	
-				var data = [[ moduleValue.xAxis.label ]];
-				
-				if (moduleValue.serieLabels && moduleValue.serieLabels.length>0) {
-					for(var i = 0, k = moduleValue.serieLabels.length; i < k; i++) {
-						data[0].push(moduleValue.serieLabels[i]);
-					}
+
+			var view = this;
+			var cfg = this.module.getConfiguration();
+
+			moduleValue = CI.DataType.getValueIfNeeded(moduleValue);	
+			var data = [[ moduleValue.xAxis.label ]];
+			
+			if (moduleValue.serieLabels && moduleValue.serieLabels.length>0) {
+				for(var i = 0, k = moduleValue.serieLabels.length; i < k; i++) {
+					data[0].push(moduleValue.serieLabels[i]);
 				}
+			}
+			
+			for(var i = 0, k = moduleValue.x.length; i < k; i++) {
+				data.push([moduleValue.x[i]]);
+			}
+			
+			for(var i = 0, k = moduleValue.series.length; i < k; i++) {
 				
-				for(var i = 0, k = moduleValue.x.length; i < k; i++) {
-					data.push([moduleValue.x[i]]);
+				for(var j = 0, l = moduleValue.series[i].length; j < l; j++) {
+					var val = moduleValue.series[i][j];		
+					if(val.value)
+						val = val.value;
+					else if(typeof val == "object")
+						val = null;
+
+					data[j + 1].push(val);	
 				}
-				
-				for(var i = 0, k = moduleValue.series.length; i < k; i++) {
+			}
+			
+			this.data = data;
+			this.chartData = google.visualization.arrayToDataTable(data);
+			var chartId = CI.Util.getNextUniqueId();
+			view.chartOptions = {
+			          title: moduleValue.title,
+			          hAxis: {title: moduleValue.xAxis.label, minValue: moduleValue.xAxis.minValue, maxValue: moduleValue.xAxis.maxValue},
+			          vAxis: {title: moduleValue.yAxis.label, minValue: moduleValue.yAxis.minValue, maxValue: moduleValue.yAxis.maxValue},
+			          legend: cfg.legend || 'none',
+			          pointSize: cfg.pointsize || 7,
+			          lineWidth: cfg.linewidth || 0
+			       };
+		       
+
+			CI.Util.DOMDeferred.progress(function(dom) {
+
+
+				if($("#" + chartId, dom).length == 0)
+						return;
+				var dom = $("#" + chartId).get(0);
+				switch(cfg.charttype) {
+
+					case 'barchart':
+					case 'vbarchart':
+						view.chart = new google
+							.visualization
+							.ColumnChart(dom);
+
+
+					break;
+					case 'hbarchart':
+						view.chart = new google
+							.visualization
+							.BarChart(dom);
+
+					break;
+					default:
+					case 'linechart': 
+						view.chart = new google
+							.visualization
+							.ScatterChart(dom);
+					break;
 					
-					for(var j = 0, l = moduleValue.series[i].length; j < l; j++) {
-						var val = moduleValue.series[i][j];		
-						if(val.value)
-							val = val.value;
-						else if(typeof val == "object")
-							val = null;
-
-						data[j + 1].push(val);	
-					}
 				}
 				
-				this.data = data;
-				this.chartData = google.visualization.arrayToDataTable(data);
-				var chartId = CI.Util.getNextUniqueId();
-				view.chartOptions = {
-				          title: moduleValue.title,
-				          hAxis: {title: moduleValue.xAxis.label, minValue: moduleValue.xAxis.minValue, maxValue: moduleValue.xAxis.maxValue},
-				          vAxis: {title: moduleValue.yAxis.label},
-				          legend: cfg.legend || 'none',
-				          pointSize: cfg.pointsize || 7,
-				          lineWidth: cfg.linewidth || 0
-				       };
-			       
+				google.visualization.events.addListener(view.chart, 'onmouseover', function(e) {
 
-				CI.Util.DOMDeferred.progress(function(dom) {
+					var row = e.row;
+					var col = e.column;
+					var rowData = moduleValue.series[col - 1][row];
 
-
-					if($("#" + chartId, dom).length == 0)
-							return;
-					var dom = $("#" + chartId).get(0);
-					switch(cfg.charttype) {
-
-						case 'barchart':
-						case 'vbarchart':
-							view.chart = new google
-								.visualization
-								.ColumnChart(dom);
-
-
-						break;
-						case 'hbarchart':
-							view.chart = new google
-								.visualization
-								.BarChart(dom);
-
-						break;
-						default:
-						case 'linechart': 
-							view.chart = new google
-								.visualization
-								.ScatterChart(dom);
-						break;
-						
-					}
-					
-					google.visualization.events.addListener(view.chart, 'onmouseover', function(e) {
-
-						var row = e.row;
-						var col = e.column;
-						var rowData = moduleValue.series[col - 1][row];
-
-						view.module.controller.hoverEvent(rowData);
-					});
-
-					view.drawChart();
-
+					view.module.controller.hoverEvent(rowData);
 				});
 
-				deferred.resolve('<div id="' + chartId + '"></div>');	
+				view.drawChart();
+
+			});
+
+			deferred.resolve('<div id="' + chartId + '"></div>');	
 		}
 	}
 }
