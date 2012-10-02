1bd47f49d3c8b70c9f2fc125b091846ad00478df
Norman Pellet <norman.pellet@gmail.com>
2012-10-02 07:08:49 +0200

    Fix ?

--- scripts/modules/implementations/spectra_displayer/1.0/view.js
+++ scripts/modules/implementations/spectra_displayer/1.0/view.js
@@ -47,7 +47,7 @@ CI.Module.prototype._types.spectra_displayer.View.prototype = {
 		'fromTo': function(moduleValue) {
 			var view = this;
 
-			if(moduleValue === undefined)
+			if(moduleValue === undefined || moduleValue.value === undefined)
 				return;
 
 			if(view.dom.data('spectra'))
